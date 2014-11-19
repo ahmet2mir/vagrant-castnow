2273d5fc3b2317e4c1868bf5e64cbb404e80ad57
ahmet2mir <ahmet2mir+github@gmail.com>
2014-11-20 00:50:33 +0100

    adding debian vagrant

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-apt-get update
-apt-get install -y --no-install-recommends curl libav-t	ools git ca-certificates
-ln -s /usr/bin/avconv /usr/bin/ffmpeg
-curl -sL https://deb.nodesource.com/setup | bash -
-apt-get install -y nodejs
-npm install castnow -g
